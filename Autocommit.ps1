aa9d8d952eba8624c865767f77537bd3c9904c79
Romain Castagne <romain.castagne40@gmail.com>
2024-10-06 01:18:55 +0200

    Commit_1

---
+++ Autocommit.ps1
@@ -0,0 +1,17 @@
+# Change to your repository directory
+Set-Location "E:\_dev\04-Scripts Powershell\Activity"
+
+# Récupérer la date actuelle
+$date = Get-Date
+
+# Ajouter une ligne au fichier README.md
+Add-Content "README.md" "`n Mise à jour du $date"
+
+# Ajouter le fichier au prochain commit
+git add .
+
+# Commit avec un message
+git commit -m "Mise à jour quotidienne du $date"
+
+# Pousser les changements vers GitHub
+git push
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Activity
 Daily activity
+
+ Mise à jour du 10/06/2024 01:16:07
+
+ Mise à jour du 10/06/2024 01:16:30
